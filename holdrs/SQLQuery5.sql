4f5380413d143899f10aa190599f8ac96f5aa1e4
Ambica Khandelwal <72697359+aashi01kh@users.noreply.github.com>
2021-10-05 23:33:24 +0530

    Add files via upload

---
+++ holdrs/SQLQuery5.sql
@@ -0,0 +1,43 @@
+
+IF OBJECT_ID('data', 'U') IS NOT NULL
+    DROP TABLE data;
+GO
+
+CREATE TABLE data(
+    
+    address VARCHAR(100),
+    balance INTEGER,
+    
+);
+GO
+
+
+
+DECLARE @data VARCHAR(MAX)
+
+SELECT @data =
+   BulkColumn
+   FROM OPENROWSET(BULK 'C:\Users\Anuj\Desktop\holdrs\generated.json', SINGLE_BLOB) JSON
+ 
+IF (ISJSON(@data)= 1)
+    BEGIN
+	     PRINT 'JSON File is valid';
+
+		 INSERT INTO data
+		 SELECT *
+		 FROM OPENJSON(@Data, '$.data.items')
+		 WITH (
+		     
+             address VARCHAR(100) '$.address',
+             balance INTEGER '$.balance'
+             
+         )
+
+    END
+
+ELSE
+    BEGIN
+	     PRINT 'JSON FILE IS INVALID' ;
+    END
+
+SELECT * FROM data
