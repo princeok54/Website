f34ed7b9340b4805cca8b4819b39762450516e93
Derrick Asamoah <asamoahderrick7@gmail.com>
2021-10-08 18:14:06 +0000

    .

--- nft/src/contracts/Color.sol
+++ nft/src/contracts/Color.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract Color is ERC721, ERC721Enumerable {
   // using Counters for Counters.Counter;
--- nft/truffle-config.js
+++ nft/truffle-config.js
@@ -41,6 +41,13 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+
+  api_keys: {
+    etherscan: process.env.BSC_API_KEY
+  },
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -118,13 +125,13 @@ module.exports = {
     solc: {
       version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"  
+      }
     }
   },
 
